ac980d5e4bd9b1f97bb976c13aa8fd13f1adc07e
Sai Kiran Goud Ravula <saikirangoud2901@gmail.com>
2024-11-06 20:55:45 -0600

    Updated with new changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/space-portfolio/', // Add this line for GitHub Pages
+  base: '/space-portfolio/', // Update this to match your repository name
 });
